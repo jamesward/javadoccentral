e7e92a1ffe7f91ea8779e591356fa5f91ad22b9f
James Ward <james@jamesward.org>
2016-04-08 17:09:38 -0600

    init

---
+++ app/controllers/MainController.scala
@@ -0,0 +1,76 @@
+package controllers
+
+import java.io.File
+import java.net.URL
+import java.nio.file.Files
+import javax.inject.Inject
+import javax.inject.Singleton
+
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
+import play.api.libs.ws.WSClient
+import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class MainController @Inject() (ws: WSClient) (implicit ec: ExecutionContext) extends Controller {
+
+  private lazy val tmpDir: File = Files.createTempDirectory("jars").toFile
+
+  private def artifactPath(groupId: String, artifactId: String, version: String): String = {
+    Seq(
+      groupId.replaceAllLiterally(".", "/"),
+      artifactId.replaceAllLiterally(".", "/"),
+      version
+    ).mkString("/")
+  }
+
+  private def downloadAndExtractZip(source: String, destination: File): Unit = {
+    val url = new URL(source)
+    val inputStream = url.openConnection().getInputStream
+    val zipArchiveInputStream = new ZipArchiveInputStream(inputStream)
+
+    Stream
+      .continually(zipArchiveInputStream.getNextEntry)
+      .takeWhile(_ != null)
+      .foreach { ze =>
+        val tmpFile = new File(destination, ze.getName)
+        if (ze.isDirectory) {
+          tmpFile.mkdirs()
+        }
+        else {
+          Files.copy(zipArchiveInputStream, tmpFile.toPath)
+        }
+      }
+
+    zipArchiveInputStream.close()
+    inputStream.close()
+  }
+
+  def index(groupId: String, artifactId: String, version: String) = Action.async {
+    val url = s"https://repo1.maven.org/maven2/${artifactPath(groupId, artifactId, version)}/"
+    ws.url(url).get().map { response =>
+      response.status match {
+        case OK =>
+          Redirect(routes.MainController.file(groupId, artifactId, version, "index.html"))
+        case _ =>
+          NotFound("The specified Maven Central module does not exist.")
+      }
+    }
+  }
+
+  def file(groupId: String, artifactId: String, version: String, file: String) = Action {
+    val path = artifactPath(groupId, artifactId, version)
+    val javadocUrl = s"https://repo1.maven.org/maven2/$path/$artifactId-$version-javadoc.jar"
+    val javadocDir = new File(tmpDir, path)
+    val javadocFile = new File(javadocDir, file)
+
+    if (!javadocDir.exists()) {
+      downloadAndExtractZip(javadocUrl, javadocDir)
+    }
+
+    Ok.sendFile(javadocFile, true)
+  }
+
+
+}
---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "javadoccentral"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  ws,
+  "org.apache.commons" % "commons-compress" % "1.11"
+)
