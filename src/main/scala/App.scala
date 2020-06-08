c147aefef9ecb3b7ede2741ec163b6b79c37a30d
James Ward <james@jamesward.com>
2020-06-08 14:59:16 -0600

    better path handling - fixes #1

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -23,7 +23,7 @@ object App extends IOApp {
   }
 
   def needArtifactId(groupId: String, maybeArtifactId: Option[String])(implicit client: Client[IO]): IO[Response[IO]] = {
-    maybeArtifactId.fold {
+    maybeArtifactId.filter(_.nonEmpty).fold {
       // todo: not found & bad request for other errors
       MavenCentral.searchArtifacts(groupId).flatMap { artifactIds =>
         Ok(html.needArtifactId(groupId, artifactIds))
@@ -34,7 +34,7 @@ object App extends IOApp {
   }
 
   def needVersion(groupId: String, artifactId: String, maybeVersion: Option[String])(implicit client: Client[IO]) = {
-    maybeVersion.fold {
+    maybeVersion.filter(_.nonEmpty).fold {
       // todo: not found & bad request for other errors
       MavenCentral.searchVersions(groupId, artifactId)(client).flatMap { versions =>
         // todo: version sorting
@@ -67,24 +67,34 @@ object App extends IOApp {
   }
 
   def file(groupId: String, artifactId: String, version: String, filepath: Path, request: Request[IO])(implicit client: Client[IO], tmpDir: File, blocker: Blocker) = {
-    val javadocUri = MavenCentral.javadocUri(groupId, artifactId, version)
-    val javadocDir = new File(tmpDir, s"$groupId/$artifactId/$version")
-    val javadocFile = new File(javadocDir, filepath.toString)
-
-    // todo: fix race condition
-    val extracted = if (!javadocDir.exists()) {
-      MavenCentral.downloadAndExtractZip(javadocUri, javadocDir)
+    if (version == "latest") {
+      MavenCentral.latest(groupId, artifactId).flatMap { maybeLatestVersion =>
+        val uri = maybeLatestVersion.fold(Uri.unsafeFromString(s"/$groupId/$artifactId")) { latestVersion =>
+          Uri.unsafeFromString(s"/$groupId/$artifactId/$latestVersion$filepath")
+        }
+        TemporaryRedirect(Location(uri))
+      }
     }
     else {
-      IO.unit
-    }
+      val javadocUri = MavenCentral.javadocUri(groupId, artifactId, version)
+      val javadocDir = new File(tmpDir, s"$groupId/$artifactId/$version")
+      val javadocFile = new File(javadocDir, filepath.toString)
 
-    extracted.flatMap { _ =>
-      if (javadocFile.exists()) {
-        StaticFile.fromFile(javadocFile, blocker, Some(request)).getOrElseF(NotFound())
+      // todo: fix race condition
+      val extracted = if (!javadocDir.exists()) {
+        MavenCentral.downloadAndExtractZip(javadocUri, javadocDir)
       }
       else {
-        NotFound("The specified file does not exist.")
+        IO.unit
+      }
+
+      extracted.flatMap { _ =>
+        if (javadocFile.exists()) {
+          StaticFile.fromFile(javadocFile, blocker, Some(request)).getOrElseF(NotFound())
+        }
+        else {
+          NotFound("The specified file does not exist.")
+        }
       }
     }
   }
@@ -94,9 +104,12 @@ object App extends IOApp {
   object OptionalVersionQueryParamMatcher extends OptionalQueryParamDecoderMatcher[String]("version")
 
   def httpApp(implicit client: Client[IO], tmpDir: File, blocker: Blocker) = HttpRoutes.of[IO] {
+    case GET -> Root / "favicon.ico" => NotFound()
     case GET -> Root :? OptionalGroupIdQueryParamMatcher(maybeGroupId) => needGroupId(maybeGroupId)
     case GET -> Root / groupId :? OptionalArtifactIdQueryParamMatcher(maybeArtifactId) => needArtifactId(groupId, maybeArtifactId)
+    case GET -> Root / groupId / "" :? OptionalArtifactIdQueryParamMatcher(maybeArtifactId) => needArtifactId(groupId, maybeArtifactId)
     case GET -> Root / groupId / artifactId :? OptionalVersionQueryParamMatcher(maybeVersion) => needVersion(groupId, artifactId, maybeVersion)
+    case GET -> Root / groupId / artifactId / "" :? OptionalVersionQueryParamMatcher(maybeVersion) => needVersion(groupId, artifactId, maybeVersion)
     case GET -> Root / groupId / artifactId / version => index(groupId, artifactId, version)
     case GET -> Root / groupId / artifactId / version / "" => index(groupId, artifactId, version)
     case req @ GET -> groupId /: artifactId /: version /: filepath => file(groupId, artifactId, version, filepath, req)
